22a2ca6d909cd3418719a94c2c3b1bf2e1d69f99
Kenkentake <keeeentake0908@gmail.com>
2023-11-04 18:25:24 +0900

    🐛 fix typo

--- terraform-sample/iam/providers.tf
+++ terraform-sample/iam/providers.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.6.3"
   backend "s3" {
     bucket = "todo-app-tfstate"
-    key    = "service/terraform.tfstate"
+    key    = "iam/terraform.tfstate"
     region = "ap-northeast-1"
   }
   required_providers {
